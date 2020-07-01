220725b09365ea7f0285990ec863e0e6872cd4ca
Davilson Junior <davilsonjr_jobs@yahoo.com>
2020-07-01 11:56:50 -0300

    configurando o servidor, o front e o mobile da aplicacao

---
+++ mobile/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: 'expo',
+  rules: {
+    'no-unused-vars': 'off',
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+  },
+};
